f9d5ac28ce097a4367e58d0bd29aee600ecdc9e9
Philipp Moritz <pcmoritz@gmail.com>
2014-05-16 21:10:12 -0700

    Integrated Entropy Function

--- src/SML.jl
+++ src/SML.jl
@@ -8,6 +8,7 @@ include("Expr.jl")
 include("functions/Modular.jl")
 include("functions/Iwata.jl")
 include("functions/GraphCut.jl")
+include("functions/LogDet.jl")
 
 include("Sort.jl")
 include("Greedy.jl")
